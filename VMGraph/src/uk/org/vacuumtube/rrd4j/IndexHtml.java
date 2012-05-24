fb1d54f0dfc2fd6691e118a0ee6cf2eb86bf440b
Clive Messer <clive@vacuumtube.org.uk>
2012-05-24 23:25:44 +0100

    Remove unused import

--- VMGraph/src/uk/org/vacuumtube/rrd4j/IndexHtml.java
+++ VMGraph/src/uk/org/vacuumtube/rrd4j/IndexHtml.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.SortedSet;
